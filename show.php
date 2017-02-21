347fcd9484d4ce2d7ae676d1a352c87dc23a9a94
htoo97 <htoo97@gmail.com>
2017-02-20 16:14:40 -0800

    State name in territory show.php

---
+++ show.php
@@ -0,0 +1,54 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+<?php
+if(!isset($_GET['id'])) {
+  redirect_to('index.php');
+}
+$id = h(strip_tags(filter_var($_GET['id'], FILTER_SANITIZE_URL)));
+$territory_result = find_territory_by_id($id);
+// No loop, only one result
+$territory = db_fetch_assoc($territory_result);
+
+// sql query returns no rows; record doesn't exist
+if (db_num_rows($territory_result) === 0) {
+  redirect_to('index.php');
+}
+
+$state_result = find_state_by_id($territory['state_id']);
+$state = db_fetch_assoc($state_result);
+
+?>
+
+<?php $page_title = 'Staff: Territory of ' . $territory['name']; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+
+<div id="main-content">
+  <a href="<?php echo '../states/show.php?id=' . u(filter_var($territory['state_id'], FILTER_SANITIZE_URL)); ?>">Back to State Details</a>
+  <br />
+
+  <h1>Territory: <?php echo $territory['name']; ?></h1>
+
+  <?php
+    echo "<table id=\"territory\">";
+    echo "<tr>";
+    echo "<td>Name: </td>";
+    echo "<td>" . $territory['name'] . "</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td>State: </td>";
+    echo "<td>" . $state['name'] . "</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td>Position: </td>";
+    echo "<td>" . $territory['position'] . "</td>";
+    echo "</tr>";
+    echo "</table>";
+
+    db_free_result($territory_result);
+  ?>
+  <br />
+  <a href="<?php echo 'edit.php?id=' . u(filter_var($id, FILTER_SANITIZE_URL)); ?>">Edit</a><br />
+
+</div>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
